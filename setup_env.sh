9b18f4d15c91a14c60e285f2591ba45fe3aae17d
Viiic98 <xathovic@gmail.com>
2020-03-29 18:58:48 +0000

    Add script that creates the environment variables

---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# To execute this script write . ./setup_env.sh
+export HBNB_ENV="test"
+export HBNB_MYSQL_USER="root"
+export HBNB_MYSQL_PWD="root"
+export HBNB_MYSQL_HOST="localhost"
+export HBNB_MYSQL_DB="hbnb_dev_db"
+export HBNB_TYPE_STORAGE="db"
