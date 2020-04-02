44ca05f2a9f31e52d1b65d2d6a6ac9f320f1314f
Victor Hernandez <vik407@gmail.com>
2020-04-01 21:24:31 -0500

    Amenity waiting for the others changes

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel):
     """This is the class for Amenity
     Attributes:
         name: input name
-    """
     name = ""
+    """
+    # FIX: amenities add
+    __tablename__ = "amenities"
+
+    name = Column(String(128),
+                  nullable=False)
+
+    place_amenities = relationship("Place",
+                                   secondary=place_amenity)
