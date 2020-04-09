1f45c937cce70c4e6aadaf743420b26d1ff19b3e
Victor Hernandez <vik407@gmail.com>
2020-04-09 22:54:55 +0000

    Fix formatting and permissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # Setup webservers for web static AirBnB_clone_V2
-
 apt-get update
-apt-get install -y nginx
-sudo ufw allow 'Nginx HTTP'
-# Create folders and and index file
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/{releases/test,shared}
 cat >> /data/web_static/releases/test/index.html << EOF
 <html>
@@ -17,10 +15,10 @@ cat >> /data/web_static/releases/test/index.html << EOF
 EOF
 # Create symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sed -i '/listen 80 default_server/a \\tlocation /hbnb_static/ {\n\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 # Adjust permissions
 chown ubuntu:ubuntu -R /data
+sed -i '/listen 80 default_server/a \\tlocation /hbnb_static/ {\n\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 # Restart nginx
-sudo service nginx restart
+service nginx restart
 # Exit with 0 code
 exit 0
