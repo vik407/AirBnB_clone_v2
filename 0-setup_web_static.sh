fc5b03ad3d03a7260a1fb922c27b1f5d3c266610
Victor Hernandez <vik407@gmail.com>
2020-04-09 15:42:33 +0000

    Permissions fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,6 @@ apt-get install -y nginx
 sudo ufw allow 'Nginx HTTP'
 # Create folders and and index file
 mkdir -p /data/web_static/{releases/test,shared}
-chown ubuntu:ubuntu -R /data
 cat >> /data/web_static/releases/test/index.html << EOF
 <html>
   <head>
@@ -19,6 +18,8 @@ EOF
 # Create symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sed -i '/listen 80 default_server/a \\tlocation /hbnb_static/ {\n\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+# Adjust permissions
+chown ubuntu:ubuntu -R /data
 # Restart nginx
 sudo service nginx restart
 # Exit with 0 code
